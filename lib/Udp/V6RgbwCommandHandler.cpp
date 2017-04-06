d18ee5e2f78d6e4dc985ce1c3e39029c8b619c2b
Chris Mullins <cmullins@liveramp.com>
2017-04-06 12:31:35 -0700

    trickle whether a button is held in UDP to RF packets

--- lib/Udp/V6RgbwCommandHandler.cpp
+++ lib/Udp/V6RgbwCommandHandler.cpp
@@ -23,8 +23,10 @@ bool V6RgbwCommandHandler::handleCommand(
     uint32_t command,
     uint32_t commandArg)
 {
-  const uint8_t cmd = command & 0xFF;
+  const uint8_t cmd = command & 0x7F;
   const uint8_t arg = commandArg >> 24;
+
+  client->setHeld((command & 0x80) == 0x80);
 
   if (cmd == V2_RGBW_COMMAND_PREFIX) {
     switch (arg) {
