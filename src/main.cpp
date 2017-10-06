f32c2abfccb12f1418de8c5c29f56d1f53613ff4
Marcel Jira <marcel@netbook-marcel.lan>
2017-10-06 22:50:48 +0200

    Added a WiFi password for security reasons. This should solve #123

--- src/main.cpp
+++ src/main.cpp
@@ -150,7 +150,8 @@ bool shouldRestart() {
 
 void setup() {
   Serial.begin(9600);
-  wifiManager.autoConnect();
+  String ssid = "ESP" + String(ESP.getChipId());
+  wifiManager.autoConnect(ssid.c_str(), "milightHub");
   SPIFFS.begin();
   Settings::load(settings);
   applySettings();
