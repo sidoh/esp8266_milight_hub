3bd32e346496ee27b816874b852bab305c78684b
cmidgley <chris@koose.com>
2018-02-15 13:33:39 -0500

    Fixed linux build and removed unwanted spaces

--- .build_web.py
+++ .build_web.py
@@ -24,20 +24,20 @@ def build_web():
                 print check_output(["npm.cmd", "install", "--only=dev"])
                 print check_output(["node_modules\\.bin\\gulp.cmd"])
             else:
-                print check_output(["npm.cmd", "install"])
+                print check_output(["npm", "install"])
                 print check_output(["node_modules/.bin/gulp"])
             copyfile("build/index.html.gz.h", "../dist/index.html.gz.h")
         except OSError as e:
-            print "Encountered error OSError building webpage: ", e
+            print "Encountered error OSError building webpage:", e
             if e.filename:
                 print "Filename is", e.filename
             print "WARNING: Failed to build web package. Using pre-built page."
         except CalledProcessError as e:
             print e.output
-            print "Encountered error CalledProcessError building webpage: ", e
+            print "Encountered error CalledProcessError building webpage:", e
             print "WARNING: Failed to build web package. Using pre-built page."
         except Exception as e:
-            print "Encountered error", type(e).__name__, "building webpage: ", e
+            print "Encountered error", type(e).__name__, "building webpage:", e
             print "WARNING: Failed to build web package. Using pre-built page."
         finally:
             os.chdir("..");
