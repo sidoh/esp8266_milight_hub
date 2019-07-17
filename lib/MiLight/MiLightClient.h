2a43093d3b7ed2fe2c726d21c85aba017f31fb35
Christopher Mullins <chris@sidoh.org>
2019-07-16 16:11:37 -1000

    Fix another goddamn typo bug

--- lib/MiLight/MiLightClient.h
+++ lib/MiLight/MiLightClient.h
@@ -29,7 +29,7 @@ namespace TransitionParams {
   static const char END_VALUE[] PROGMEM = "end_value";
   static const char DURATION[] PROGMEM = "duration";
   static const char PERIOD[] PROGMEM = "period";
-  static const char NUM_PERIODS[] PROGMEM = "period";
+  static const char NUM_PERIODS[] PROGMEM = "num_periods";
 }
 
 // Used to determine RGB colros that are approximately white
