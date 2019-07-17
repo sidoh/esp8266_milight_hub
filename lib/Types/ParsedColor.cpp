65777390d6f7aa3b31f07917e3100caea977973d
Christopher Mullins <chris@sidoh.org>
2019-07-16 16:28:22 -1000

    This is a bug -- do not extract color field, the JsonVarient will already be the value from this field.

--- lib/Types/ParsedColor.cpp
+++ lib/Types/ParsedColor.cpp
@@ -25,7 +25,7 @@ ParsedColor ParsedColor::fromJson(JsonVariant json) {
   uint16_t r, g, b;
 
   if (json.is<JsonObject>()) {
-    JsonObject color = json[GroupStateFieldNames::COLOR];
+    JsonObject color = json.as<JsonObject>();
 
     r = color["r"];
     g = color["g"];
