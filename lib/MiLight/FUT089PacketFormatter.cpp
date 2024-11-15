06c82ac795634c4c1e2c906a3cdab2542555e552
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2024-11-15 20:55:04 +0100

    Use fut092 commands for saturation and temperature

--- lib/MiLight/FUT089PacketFormatter.cpp
+++ lib/MiLight/FUT089PacketFormatter.cpp
@@ -29,58 +29,14 @@ void FUT089PacketFormatter::updateColorRaw(uint8_t value) {
   command(FUT089_COLOR, FUT089_COLOR_OFFSET + value);
 }
 
-// change the temperature (kelvin).  Note that temperature and saturation share the same command
-// number (7), and they change which they do based on the mode of the lamp (white vs. color mode).
-// To make this command work, we need to switch to white mode, make the change, and then flip
-// back to the original mode.
+// change the temperature (kelvin). Update: now uses the fut092 command to skip mode changes
 void FUT089PacketFormatter::updateTemperature(uint8_t value) {
-  // look up our current mode
-  const GroupState* ourState = this->stateStore->get(this->deviceId, this->groupId, REMOTE_TYPE_FUT089);
-  BulbMode originalBulbMode;
-
-  if (ourState != NULL) {
-    originalBulbMode = ourState->getBulbMode();
-
-    // are we already in white?  If not, change to white
-    if (originalBulbMode != BulbMode::BULB_MODE_WHITE) {
-      updateColorWhite();
-    }
-  }
-
-  // now make the temperature change
-  command(FUT089_KELVIN_SATURATION, 100 - value);
-
-  // and return to our original mode
-  if (ourState != NULL && (settings->enableAutomaticModeSwitching) && (originalBulbMode != BulbMode::BULB_MODE_WHITE)) {
-    switchMode(*ourState, originalBulbMode);
-  }
+  command(FUT092_KELVIN, 100 - value);
 }
 
-// change the saturation.  Note that temperature and saturation share the same command
-// number (7), and they change which they do based on the mode of the lamp (white vs. color mode).
-// Therefore, if we are not in color mode, we need to switch to color mode, make the change,
-// and switch back to the original mode.
+// change the saturation. Update: now uses the fut092 command to skip mode changes
 void FUT089PacketFormatter::updateSaturation(uint8_t value) {
-  // look up our current mode
-  const GroupState* ourState = this->stateStore->get(this->deviceId, this->groupId, REMOTE_TYPE_FUT089);
-  BulbMode originalBulbMode = BulbMode::BULB_MODE_WHITE;
-
-  if (ourState != NULL) {
-    originalBulbMode = ourState->getBulbMode();
-  }
-
-  // are we already in color?  If not, we need to flip modes
-  if (ourState != NULL && (settings->enableAutomaticModeSwitching) && (originalBulbMode != BulbMode::BULB_MODE_COLOR)) {
-    updateHue(ourState->getHue());
-  }
-
-  // now make the saturation change
-  command(FUT089_KELVIN_SATURATION, 100 - value);
-
-  // and revert back if necessary
-  if (ourState != NULL && (settings->enableAutomaticModeSwitching) && (originalBulbMode != BulbMode::BULB_MODE_COLOR)) {
-    switchMode(*ourState, originalBulbMode);
-  }
+  command(FUT092_SATURATION, 100 - value);
 }
 
 void FUT089PacketFormatter::updateColorWhite() {
